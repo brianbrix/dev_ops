f69ea7fd089c7492f73a563192685deca76a205b
brianbrix <mokandubria@gmail.com>
2021-03-04 14:28:57 +0300

    Add scripts to enable nodes to communicate with each other

--- local.py
+++ local.py
@@ -16,12 +16,11 @@ with open('server.txt', 'r') as infile:
             dix.append([x.strip() for x in i.split(",")])
 for x in dix:
     pass_arg = ["sshpass", "-p", x[1], "ssh-copy-id", x[0]]
-#    print(x[0] + ":~")
 
     p = subprocess.Popen(pass_arg, stdin=subprocess.PIPE, shell=False)
     p.communicate(input='\nyes\n{}\n'.format(x[1]).encode())
-
-    p = subprocess.Popen(["scp", "-r", "/home/brianbrix/PycharmProjects/devops/test3.py", x[0] + ":~"])
+    #upload the pythpon and servers list to remote machine
+    p = subprocess.Popen(["scp", "-r", "/home/brianbrix/PycharmProjects/devops/remote.py", x[0] + ":~"])
     sts = os.waitpid(p.pid, 0)
     p.communicate(input='\nyes\ny\n'.encode())
 
@@ -29,15 +28,13 @@ for x in dix:
     sts = os.waitpid(p.pid, 0)
     p.communicate(input='\nyes\ny\n'.encode())
 
-    # print(subprocess.run("ssh {} {}".format(x[0], 'bash -s {}'.format(x[2].split('/')[-1])), shell=True,
-    #                  ).stdout)
-#    print('sudo -S bash {}'.format(x[2].split('/')[-1]))
-#    result = subprocess.run(['ssh', x[0], f"echo {x[1]} | sudo -S bash {x[2].split('/')[-1]}"]).stdout
+
     os_t = subprocess.check_output(["uname", "-a"]).decode()
 #    print(os_t, "#################")
     result = subprocess.run(['ssh', x[0], f"echo {x[1]} | sudo -S apt install sshpass"]).stdout
     print("running python")
-    result = subprocess.run(['ssh', x[0], f"python3 test3.py"]).stdout
+    #run python script in remote machine
+    result = subprocess.run(['ssh', x[0], f"python3 remote.py"]).stdout
     result = subprocess.run(['pwd']).stdout
     print(x[0], "completed")
 
